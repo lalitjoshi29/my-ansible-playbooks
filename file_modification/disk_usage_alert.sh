9742c3f1c3a19d555ea0eaa3a7142cc13a6fcc27
Lalit <lmjsbooks@gmail.com>
2019-08-20 13:32:41 +0530

    Made some formatting in disk usage monitoring scripts

--- file_modification/disk_usage_alert.sh
+++ file_modification/disk_usage_alert.sh
@@ -7,21 +7,25 @@
 # Shell script to monitor or watch the disk space 
 #################################################################
 
+
 # set alert level 85% is default 
 ALERT=85 
 # 
+##################
 
 function main_prog() { 
      echo "$(hostname)," 
-while read output; 
-do 
-  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1) 
-  partition=$(echo $output | awk '{print $2}') 
-  if [ $usep -ge $ALERT ] ; then 
-     echo "$partition,$usep%" 
-  fi 
-done 
+     while read output; 
+     do 
+        usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1) 
+        partition=$(echo $output | awk '{print $2}') 
+           if [ $usep -ge $ALERT ] ; then 
+                 echo "$partition,$usep%" 
+           fi 
+     done 
 } 
+
+#################
 
 
 df -PHT | grep -vE "^Filesystem|tmpfs|cdrom|nfs|cifs" | awk '{print $6 " " $7}' | main_prog 
--- file_modification/disk_usage_monitoring_script_example_1.sh
+++ file_modification/disk_usage_monitoring_script_example_1.sh
@@ -1,26 +1,24 @@
+#######################################################
 #!/bin/bash 
-
+#
+#################
 SERVER_LIST=/var/tmp/testdev_server_list.txt 
 INPUT_SCRIPT=/etc/scripts/disk_usage_alert.sh 
 OUTPUT_FILE=/var/tmp/disk_usage_alert_output.csv 
 OUTPUT_TEMP_FILE=/var/tmp/disk_usage_alert_output_temp.csv 
 MAIL_RECIPIENTS="xyz@example.com" 
 
+#################
 for server in `cat $SERVER_LIST` 
-
   do 
-
     ssh $server 'bash -s' < $INPUT_SCRIPT 
-
   done > $OUTPUT_TEMP_FILE 
 
 grep "%$" -B 1 $OUTPUT_TEMP_FILE | grep -v ^- > $OUTPUT_FILE 
 grep -q "%" $OUTPUT_FILE 
 
 if [ $? -eq 0 ] 
-
 then 
-
   echo "Disk usage alert for UNIX TEST and DEV servers" | mailx -s "Disk usage alert for UNIX TEST and DEV servers" -a $OUTPUT_FILE $MAIL_RECIPIENTS  < $OUTPUT_FILE 
 
 fi 
