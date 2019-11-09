0872b14b60e6323a1bb68396050e580a4db5248f
Lalit <lmjsbooks@gmail.com>
2019-11-09 21:30:18 +0530

    updated user data file

--- ansible_lab_setup_using_ec2/user_data_for_ec2.sh
+++ ansible_lab_setup_using_ec2/user_data_for_ec2.sh
@@ -4,3 +4,11 @@ sudo usermod -a -G wheel devops
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl restart sshd.service
 
+
+
+
+##!/bin/bash
+#useradd devops --create-home --password "$(openssl passwd -1 "redhat")" --shell /bin/bash
+#echo "devops   ALL=(ALL)   NOPASSWD: ALL" > /etc/sudoers.d/ansible
+#sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+#systemctl restart sshd.service
